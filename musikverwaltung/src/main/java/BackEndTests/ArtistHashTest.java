f5d5dc22a7683b663553d41c88029b848f71ef34
pGManiac <konradkomplexeleistung@gmail.com>
2023-07-04 14:11:30 +0200

    MV011
    Added extra layer to Datastructure: Hashmaps are now stored in a Database object. Added functionality to save Database as .ser File. Added Tests to Database Class. Added Tests to ArtistHashTest

---
+++ musikverwaltung/src/main/java/BackEndTests/ArtistHashTest.java
@@ -0,0 +1,6 @@
+package BackEndTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ArtistHashTest {
+  
+}
---
+++ musikverwaltung/src/main/java/BackEndTests/DatabaseTest.java
@@ -0,0 +1,7 @@
+package BackEndTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+}
