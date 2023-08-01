feb6a71f3a4811a7efd5e3b3d8f3f11683644b54
trisselino <tristan.sopauschke@gmail.com>
2023-08-01 20:33:00 +0200

    MV037
    Added Genre selection to Displaymode

--- musikverwaltung/src/main/java/frontend/ChooseGenreGUI.java
+++ musikverwaltung/src/main/java/frontend/ChooseGenreGUI.java
@@ -1,5 +1,6 @@
 package frontend;
 import backend.Database;
+import backend.GenreHash;
 import backend.Song;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,14 +10,13 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChooseGenreGUI {
     private Database data;
-    private TableView<Song> albums;
-    private TableColumn<Song, String> albumColumn;
-    private TableColumn<Song, String> artistColumn;
-
+    private TableView<Song> tableview;
+    private TableColumn<Song, String> genreColumn;
     private MediaPlaylist mediaPlaylist;
     private Dialog<Song> adder;
     private ObservableList<Song> tableData;
@@ -28,23 +28,20 @@ public class ChooseGenreGUI {
 
     public void selectGenre() {
         // *** GUI ***
-        albums = new TableView<>();
-        tableData = FXCollections.observableList(data.getAlbumHash().getOneSongPerAlbum());
+        tableview = new TableView<>();
+        tableData = FXCollections.observableList(this.getOneSongFromEachRepresentedGenre());
 
         adder = new Dialog<>();
-        adder.setTitle("Albumwahl");
-        Label header = new Label("Wähle ein Album!");
-        header.setId("albumSelectionHeader");
+        adder.setTitle("Genrewahl");
+        Label header = new Label("Wähle ein Genre aus!");
+        header.setId("genreSelectionHeader");
         adder.setResizable(true);
 
-        albumColumn = new TableColumn<>("Album");
-        artistColumn = new TableColumn<>("Artist");
-        albumColumn.setCellValueFactory(new PropertyValueFactory<>("album"));
-        artistColumn.setCellValueFactory(new PropertyValueFactory<>("artist"));
-        albums.getColumns().add(albumColumn);
-        albums.getColumns().add(artistColumn);
-        albums.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        albums.setItems(tableData);
+        genreColumn = new TableColumn<>("Genres");
+        genreColumn.setCellValueFactory(new PropertyValueFactory<>("genreName"));
+        tableview.getColumns().add(genreColumn);
+        tableview.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tableview.setItems(tableData);
 
         DialogPane diaPane = adder.getDialogPane();
         diaPane.getStylesheets().add((new File("src/main/java/frontend/VerwaltungGUI.css")).toURI().toString());
@@ -54,21 +51,21 @@ public class ChooseGenreGUI {
         ButtonType okButtonType = new ButtonType("Fertig", ButtonBar.ButtonData.OK_DONE);
         BorderPane diaBorder = new BorderPane();
 
-        StackPane stack = new StackPane(albums);
+        StackPane stack = new StackPane(tableview);
         diaBorder.setCenter(stack);
         diaBorder.setTop(header);
         diaBorder.setBottom(chooseAlbumButton);
 
         chooseAlbumButton.setOnAction(e -> {
-            Song selectedAlbum = albums.getSelectionModel().getSelectedItem();
-            if (selectedAlbum != null) {
-                List<Song> selectedSongs = data.getAlbumHash().getAllSongsFromAlbum(selectedAlbum);
+            Song selectedGenre = tableview.getSelectionModel().getSelectedItem();
+            if (selectedGenre != null) {
+                List<Song> selectedSongs = data.getGenreHash().getSongsFromGenre(selectedGenre.getGenreNumber());
                 mediaPlaylist.setSongs(selectedSongs);
                 // Create and show an information alert to display the notification
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Album hinzugefügt");
+                alert.setTitle("Genre hinzugefügt");
                 alert.setHeaderText(null);
-                alert.setContentText("Das Album '" + selectedAlbum.getAlbum() + "' wurde der Playlist hinzugefügt!");
+                alert.setContentText("Alle Songs des Genres '" + selectedGenre.getAlbum() + "' wurden der Playlist hinzugefügt!");
                 alert.showAndWait();
             }
         });
@@ -77,5 +74,18 @@ public class ChooseGenreGUI {
         adder.getDialogPane().getButtonTypes().add(okButtonType);
         adder.getDialogPane().setPrefSize(900, 800);
         adder.showAndWait();
+    }
+
+    private List<Song> getOneSongFromEachRepresentedGenre() {
+        boolean[] usedGenreList = data.getGenreHash().genresRepresented();
+        List<Song> oneSongFromEachGenre = new ArrayList<>();
+
+        for (int i = 0; i <= 6; i++) {
+            if (usedGenreList[i]) {
+                // adds the first Song in the List of Songs from Genre to the list
+                oneSongFromEachGenre.add(data.getGenreHash().getSongsFromGenre(i).get(0));
+            }
+        }
+        return oneSongFromEachGenre;
     }
 }
